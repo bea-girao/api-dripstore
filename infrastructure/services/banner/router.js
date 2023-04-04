abdbfc59913bb086156a62597635202f3c0dd4ad
Beatriz <anabeatrizgirao@gmail.com>
2023-04-04 13:15:29 -0300

    orm

--- infrastructure/services/banner/router.js
+++ infrastructure/services/banner/router.js
@@ -3,7 +3,15 @@ const Banner = require('../../entity/banner');
 const orm = require('../../connection/orm');
 
 app.get('/banners', async (req, res) => {
-    res.send('ok');
+    let dados = await Banner.findAll();
+
+    res.send(dados);
+});
+
+app.get('/banners/:id', async (req, res) => {
+    let dados = await Banner.findByPk(req.params.id);
+
+    res.send(dados);
 });
 
 module.exports = app;
